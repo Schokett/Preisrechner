5918f51079b31e7798c7807e8fad21ed7d429cd5
schokett <skrollanelenazimmermann@gmail.com>
2026-04-28 00:18:34 +0200

    parameters added + function selectItems

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 function selectItems(index) {
+  //holt nur Preis
   const element = document.getElementsByClassName("show-amount");
+  const price = element[index].innerText;
+  console.log(price);
 
-  const text = element[index].innerText;
+  //holt nur den Namen
+  const element2 = document.getElementsByClassName("text");
+  const fullText = element2[index].innerText;
+  const product = fullText.split("\n")[0];
+  console.log(product);
 
-  console.log(text);
+  //holt Text und Preis
+  //   const element2 = document.getElementsByClassName("text");
+  //   const product = element2[index].innerText;
+  //   console.log(product);
 }
