1fad78872064e91bd714b2e9c7f4e185cce18f85
schokett <skrollanelenazimmermann@gmail.com>
2026-04-28 00:37:49 +0200

    script added

--- script.js
+++ script.js
@@ -1,17 +1,33 @@
-function selectItems(index) {
-  //holt nur Preis
-  const element = document.getElementsByClassName("show-amount");
-  const price = element[index].innerText;
-  console.log(price);
+// let totaPrice = 0;
 
-  //holt nur den Namen
-  const element2 = document.getElementsByClassName("text");
-  const fullText = element2[index].innerText;
-  const product = fullText.split("\n")[0];
-  console.log(product);
+// function selectItems(index) {
+//   //holt nur Preis
+//   const elemente = document.getElementsByClassName("show-amount");
 
-  //holt Text und Preis
-  //   const element2 = document.getElementsByClassName("text");
-  //   const product = element2[index].innerText;
-  //   console.log(product);
-}
+//   let preisAlsText = elemente[index].getAttribute("value");
+
+//   let preisAlsZahl = preisAlsText.replace(",", ".");
+
+//   let preis = parseFloat(preisAlsZahl);
+
+//   totaPrice = totaPrice + preis;
+
+//   console.log("Ausgewählt: " + preis.toFixed(2) + "€");
+//   console.log("Neue totaPrice: " + totaPrice.toFixed(2) + "€");
+
+//   //   const price = element[index].innerText;
+//   //   console.log(price);
+
+//   //holt nur den Namen
+//   //   const element2 = document.getElementsByClassName("text");
+//   //   const fullText = element2[index].innerText;
+//   //   const product = fullText.split("\n")[0];
+//   //   console.log(product);
+
+//   //holt Text und Preis
+//   //   const element2 = document.getElementsByClassName("text");
+//   //   const product = element2[index].innerText;
+//   //   console.log(product);
+// }
+
+//eigene Lösung:
